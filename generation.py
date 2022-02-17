2c74efcc57b55df902fb40b6074051b3cddb8eb6
megavaz <fruitpizza49@gmail.com>
2022-02-17 10:39:52 +0300

    add notebooks, reformat generation.py

--- generation.py
+++ generation.py
@@ -5,10 +5,13 @@ import argparse
 import numpy as np
 import os
 from tqdm import tqdm
-base_path = os.path.abspath('')
+
+base_path = os.path.abspath("")
 
 
-def split_and_predict(x, model, split=(8, 8, 1), overlap=1 / 3, cutoff=5, batch_size=24):
+def split_and_predict(
+    x, model, split=(8, 8, 1), overlap=1 / 3, cutoff=5, batch_size=24
+):
     def gen_restore_data(x_train_windows):
         def gen():
             for i in range(x_train_windows.shape[0]):
@@ -36,18 +39,19 @@ def split_and_predict(x, model, split=(8, 8, 1), overlap=1 / 3, cutoff=5, batch_
         x = np.pad(x, ((0, pad_values[0]), (0, pad_values[1]), (0, 0)))
         y = np.zeros_like(x)
         y_coef = np.zeros_like(x)
-        window_coef = np.full((patch_size, patch_size, 1), 10E-5)
+        window_coef = np.full((patch_size, patch_size, 1), 10e-5)
         window_coef[..., 0] = get_window(window_coef[...])
         x_train_windows = view_as_windows(x, (patch_size, patch_size, 1), step=step)
 
         dataset = tf.data.Dataset.from_generator(
             gen_restore_data(x_train_windows),
-            output_signature=tf.TensorSpec(shape=(patch_size, patch_size, 1)))
+            output_signature=tf.TensorSpec(shape=(patch_size, patch_size, 1)),
+        )
         dataset = dataset.batch(batch_size)
         try:
             res = model.predict(dataset, verbose=0)
         except ValueError:
-            return y[:orig_shape[0], :orig_shape[1]]
+            return y[: orig_shape[0], : orig_shape[1]]
         counter = 0
         y_windows = view_as_windows(y, (patch_size, patch_size, 1), step=step)
         y_coef_windows = view_as_windows(y_coef, (patch_size, patch_size, 1), step=step)
@@ -58,8 +62,8 @@ def split_and_predict(x, model, split=(8, 8, 1), overlap=1 / 3, cutoff=5, batch_
                     y_windows[i, j, 0] += res[counter] * window_coef
                     counter += 1
                 y_coef_windows[i, j, 0] += window_coef
-        y = y / (y_coef + 10E-5)
-        return y[:orig_shape[0], :orig_shape[1]]
+        y = y / (y_coef + 10e-5)
+        return y[: orig_shape[0], : orig_shape[1]]
 
     step = np.array(x.shape) // np.array(split)
     overlap = step * overlap
@@ -74,60 +78,81 @@ def split_and_predict(x, model, split=(8, 8, 1), overlap=1 / 3, cutoff=5, batch_
     j_s_l = [i * step[1] for i in range(split[1] + 2)]
     i_s_r = [i + overlap[0] for i in i_s_l[1:]]
     j_s_r = [i + overlap[1] for i in j_s_l[1:]]
-    window_coef = get_window(np.zeros_like(x[:i_s_r[0], :j_s_r[0]]))[..., np.newaxis]
+    window_coef = get_window(np.zeros_like(x[: i_s_r[0], : j_s_r[0]]))[..., np.newaxis]
     window_aux = np.zeros_like(window_coef)
     window_aux[cutoff:-cutoff, cutoff:-cutoff] = 1
     np.multiply(window_coef, window_aux, out=window_coef)
     window_aux = None
     for i_l, i_r in tqdm(zip(i_s_l[:-1], i_s_r), total=len(i_s_r)):
         for j_l, j_r in zip(j_s_l[:-1], j_s_r):
-            temp = perform_transfer(x[i_l: i_r, j_l: j_r])
+            temp = perform_transfer(x[i_l:i_r, j_l:j_r])
             np.multiply(temp, window_coef, out=temp)
-            np.add(y[i_l: i_r, j_l: j_r], temp, out=y[i_l: i_r, j_l: j_r])
-            np.add(coefs[i_l: i_r, j_l: j_r], window_coef, coefs[i_l: i_r, j_l: j_r])
-    np.add(coefs, 10E-5, out=coefs)
+            np.add(y[i_l:i_r, j_l:j_r], temp, out=y[i_l:i_r, j_l:j_r])
+            np.add(coefs[i_l:i_r, j_l:j_r], window_coef, coefs[i_l:i_r, j_l:j_r])
+    np.add(coefs, 10e-5, out=coefs)
     np.divide(y, coefs, out=y)
-    return y[cutoff: orig_shape[0] + cutoff, cutoff: orig_shape[1] + cutoff]
+    return y[cutoff : orig_shape[0] + cutoff, cutoff : orig_shape[1] + cutoff]
 
 
 def main():
-    clip = 2000.
+    clip = 2000.0
     parser = argparse.ArgumentParser(
-        description='Use the script to generate DMSP-like data from VIIRS data. Shape of the file should be (height, '
-                    'width, 1) where 1 is the number of channels.')
-    parser.add_argument('path', type=str, help='Path to the file you want to perform translation on. '
-                                               'Note, that if you want to process .envi file you need to use'
-                                               " --header argument and set the path to the file's header. "
-                                               "Otherwise your file is considered to be in .npz format")
-    parser.add_argument('--header', type=str, default=None)
-    parser.add_argument('--split', type=int, default=8, help='Number of tiles to split the imagery into.')
-    parser.add_argument('--batch_size', type=int, default=16, help='Number of batches to be used in the prediction '
-                                                                   'process.')
-    parser.add_argument('--model', type=str, default='/model/model_MAE_depth_4_per_level_3.h5', help='Path to the model')
+        description="Use the script to generate DMSP-like data from VIIRS data. Shape of the file should be (height, "
+        "width, 1) where 1 is the number of channels."
+    )
+    parser.add_argument(
+        "path",
+        type=str,
+        help="Path to the file you want to perform translation on. "
+        "Note, that if you want to process .envi file you need to use"
+        " --header argument and set the path to the file's header. "
+        "Otherwise your file is considered to be in .npz format",
+    )
+    parser.add_argument("--header", type=str, default=None)
+    parser.add_argument(
+        "--split",
+        type=int,
+        default=8,
+        help="Number of tiles to split the imagery into.",
+    )
+    parser.add_argument(
+        "--batch_size",
+        type=int,
+        default=16,
+        help="Number of batches to be used in the prediction " "process.",
+    )
+    parser.add_argument(
+        "--model",
+        type=str,
+        default="/model/model_MAE_depth_4_per_level_3.h5",
+        help="Path to the model",
+    )
 
     args = parser.parse_args()
     if args.header is None:
-        img = np.load(base_path + '/' + args.path)
-        img = img['arr_0']
+        img = np.load(base_path + "/" + args.path)
+        img = img["arr_0"]
     else:
         import spectral.io.envi as envi
-        img = envi.open(base_path + '/' + args.header,
-                        base_path + '/' + args.path)
+
+        img = envi.open(base_path + "/" + args.header, base_path + "/" + args.path)
         img = img.load()
     img = np.clip(img, 0, clip)
     img = img / img.max()
 
-    model = tf.keras.models.load_model(base_path + '/' + args.model)
+    model = tf.keras.models.load_model(base_path + "/" + args.model)
     model.compile()
 
     if len(img.shape) == 2:
         img = img[..., np.newaxis]
-    res = split_and_predict(img, model, (int(args.split), int(args.split), 1), batch_size=args.batch_size)
+    res = split_and_predict(
+        img, model, (int(args.split), int(args.split), 1), batch_size=args.batch_size
+    )
     res = res * 63
     res = np.around(res, 0)
-    np.savez(base_path + '/result.npz', res)
-    print('Finished restoration!')
+    np.savez(base_path + "/result.npz", res)
+    print("Finished restoration!")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
---
+++ notebooks/README.md
@@ -0,0 +1,4 @@
+Research notebooks with architecture. They are loaded from colab, so
+they need to be changed to work on your machine.
+
+In case of any questions contact me on nechaev.dv@phystech.edu
